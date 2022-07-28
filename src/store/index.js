9934c4e5f7c5791b46d7b00c313dc875386da669
izim007 <106978346+izim007@users.noreply.github.com>
2022-07-28 15:20:48 +0900

    데스크탑 index.js 원래 파일로 원복
    데스크탑 오류로 index.js 원래 파일로 원복

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import recipe from './modules/recipe'
+import user from './modules/user'
+import filter from './modules/filter'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    // recipe
+    user: user,
+    filter: filter,
   }
 })
