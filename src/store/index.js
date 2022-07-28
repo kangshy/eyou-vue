fa7285ab03a5bb41ae8ae068bfb12643136648e4
izim007 <106978346+izim007@users.noreply.github.com>
2022-07-28 15:01:41 +0900

    내 데스크탑에 설치한 모듈 커밋
    내 데스크탑에 설치한 모듈에 대한 수정사항 커밋

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// import recipe from './modules/recipe'
-import user from './modules/user'
-import filter from './modules/filter'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
   modules: {
-    // recipe
-    user: user,
-    filter: filter,
   }
 })
